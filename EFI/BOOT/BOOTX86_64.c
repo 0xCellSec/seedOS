bd2d56c420c2dd8184551c3da6f25eb7054c07e7
serenity-cell <justin1726hernandez2@gmail.com>
2026-06-27 20:11:14 +0000

    written a small todo list

--- EFI/BOOT/BOOTX86_64.c
+++ EFI/BOOT/BOOTX86_64.c
@@ -6,7 +6,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   InitializeLib(ImageHandle, SystemTable);
   SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
   
-  Print(L"Seed OS by NM, well you can change the name!\n");
+  Print(L"Welcome user to Seed OS\n");
   Print(L"Press any key to continue...\n");
 
   // Wait for a key press
@@ -18,6 +18,9 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &EventIndex);
   SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
   
- 
+  // TODO
+  // 1. prepare resources in memory 
+  // 2. pass vital tables like the graphics buffer
+  // 3. exit the uefi boot service by gettint the memory map and calling ExitBootServices
   return EFI_SUCCESS;
 }
