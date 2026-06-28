d40782d0d98165f83facd60b7113afc396b35aa8
serenity-cell <justin1726hernandez2@gmail.com>
2026-06-28 18:52:44 +0000

    refactoring MemoryMap
    move all of the GetMemoryMap and memory map code into its own function along with out-param the variables

--- EFI/BOOT/BOOTX86_64.c
+++ EFI/BOOT/BOOTX86_64.c
@@ -1,29 +1,21 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+EFI_STATUS GetSystemMemoryMap(EFI_SYSTEM_TABLE *SystemTable,  UINTN *mapSize, UINTN *mapKey, UINTN *descriptorSize, 
+                              UINT32 *descriptorVersion, EFI_MEMORY_DESCRIPTOR **MemoryMap)
 {
-  InitializeLib(ImageHandle, SystemTable);
-  SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
 
-  Print(L"Welcome user to Seed OS\n");
-  // MEMORY MAP
-  UINTN mapSize = 0;
-  UINTN mapKey = 0;
-  UINTN descriptorSize = 0;
-  UINT32 descriptorVersion = 0;
-  EFI_MEMORY_DESCRIPTOR *MemoryMap = NULL;
+  EFI_STATUS status = SystemTable->BootServices->GetMemoryMap(mapSize, NULL, mapKey, descriptorSize, descriptorVersion);
 
-  EFI_STATUS status = SystemTable->BootServices->GetMemoryMap(&mapSize, NULL, &mapKey, &descriptorSize, &descriptorVersion);
-
-  if (status == EFI_BUFFER_TOO_SMALL) {
-    Print(L"Memory Map Size: %lu\n", mapSize);
-    mapSize += descriptorSize * 8;
-    status = SystemTable->BootServices->AllocatePool(EfiLoaderData, mapSize, (void**)&MemoryMap);
+  if (status == EFI_BUFFER_TOO_SMALL) 
+  {
+    Print(L"Memory Map Size: %lu\n", *mapSize);
+    *mapSize += *descriptorSize * 8;
+    status = SystemTable->BootServices->AllocatePool(EfiLoaderData, *mapSize, (void**)MemoryMap);
     
   }
 
-  status = SystemTable->BootServices->GetMemoryMap(&mapSize, MemoryMap, &mapKey, &descriptorSize, &descriptorVersion);
+  status = SystemTable->BootServices->GetMemoryMap(mapSize, *MemoryMap, mapKey, descriptorSize, descriptorVersion);
 
   if (EFI_ERROR(status))
   {
@@ -31,7 +23,31 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     return status;
       
   }
+  return EFI_SUCCESS;
 
+}
+
+
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+  InitializeLib(ImageHandle, SystemTable);
+  SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
+  
+  UINTN mapSize = 0;
+  UINTN mapKey = 0;
+  UINTN descriptorSize = 0;
+  UINT32 descriptorVersion = 0;
+  EFI_MEMORY_DESCRIPTOR *MemoryMap = NULL;
+
+
+  Print(L"Welcome user to Seed OS\n");
+  
+  EFI_STATUS status = GetSystemMemoryMap(SystemTable, &mapSize, &mapKey, &descriptorSize, &descriptorVersion, &MemoryMap);
+   if (EFI_ERROR(status))
+  {
+    Print(L"Something went wrong: %r\n", status);
+    return status;
+  }
   Print(L"Press any key to continue...\n");
 
   // Wait for a key press
