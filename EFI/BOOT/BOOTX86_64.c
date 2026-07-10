d9c4ecac578ed5d34c70f7fc6332ab223ccda67b
serenity-cell <justin1726hernandez2@gmail.com>
2026-07-10 21:58:03 +0000

    refactoring efi_main
    refactoring seperated into KernelLoader and ElfMagicCheck

--- EFI/BOOT/BOOTX86_64.c
+++ EFI/BOOT/BOOTX86_64.c
@@ -3,6 +3,8 @@
 
 #define PT_LOAD 1
 
+EFI_STATUS status;
+
 // elf header struct
 struct elf64_ehdr{
   unsigned char e_ident[16];
@@ -40,7 +42,7 @@ struct elf64_phdr {
 EFI_STATUS GetSystemMemoryMap(EFI_SYSTEM_TABLE *SystemTable,  UINTN *mapSize, UINTN *mapKey, UINTN *descriptorSize, 
                               UINT32 *descriptorVersion, EFI_MEMORY_DESCRIPTOR **MemoryMap)
 {
-  EFI_STATUS status = SystemTable->BootServices->GetMemoryMap(mapSize, NULL, mapKey, descriptorSize, descriptorVersion);
+  status = SystemTable->BootServices->GetMemoryMap(mapSize, NULL, mapKey, descriptorSize, descriptorVersion);
 
   if (status == EFI_BUFFER_TOO_SMALL) 
   {
@@ -62,9 +64,78 @@ EFI_STATUS GetSystemMemoryMap(EFI_SYSTEM_TABLE *SystemTable,  UINTN *mapSize, UI
 
 }
 
+EFI_STATUS KernelLoader( EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable, EFI_PHYSICAL_ADDRESS *buffer, UINTN kernelSize, UINTN pages) {  
+   //loading kernel declerations
+  EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
+  EFI_FILE_PROTOCOL *Root;
+  EFI_FILE_PROTOCOL *KernelFile;
+  EFI_FILE_INFO *fileInfo;
+
+  EFI_STATUS status;
+
+  // loading the kernel
+  Print(L"loading kernel onto memory\n");
+  status = SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
+  if (EFI_ERROR(status)) {
+    Print(L"something went wrong in kernel loader\n", status);
+  }
+  Print(L"loaded image\n");
+  
+  status = SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem);
+  if (EFI_ERROR(status)) {
+    Print(L"something went wrong in kernel loader\n", status);
+  }
+  Print(L"passed image to file system\n");
+  
+  FileSystem->OpenVolume(FileSystem, &Root);
+  if (EFI_ERROR(status)) {
+    Print(L"something went wrong in kernel loader\n", status);
+  }
+  Print(L"opened the file\n");
+  
+  status = Root->Open(Root, &KernelFile, L"kernel\\kernel.elf", EFI_FILE_MODE_READ, 0);
+  if (EFI_ERROR(status)) {
+    Print(L"something went wrong in kernel loader\n", status);
+  }
+  Print (L"set file to read mode\n");
+
+  fileInfo = LibFileInfo(KernelFile);
+  if (fileInfo == NULL) {
+    Print(L"LibFileInfo returned NULL\n");
+  }
+  Print(L"got file info\n");
+  kernelSize = fileInfo->FileSize; 
+  Print(L"captured kernel size from info\n");
+  pages = (kernelSize + 0xFFF) / 0x1000;
+  SystemTable->BootServices->AllocatePages(AllocateAnyPages, EfiLoaderData, pages, buffer);
+  KernelFile->Read(KernelFile, &kernelSize, (void *)buffer);
+  Print(L"Kernel loaded\n");
+
+  return EFI_SUCCESS;
+}
+
+EFI_STATUS ElfMagicCheck (struct elf64_ehdr *elf) {
+  Print(L"checking ELF magic numbers");
+
+  // checking for the elf magic numbers
+    if (elf->e_ident[0] == 0x7F && elf->e_ident[1] == 'E' && elf->e_ident[2] == 'L' 
+      && elf->e_ident[3] == 'F'){
+    Print(L"File validated to be an ELF executable\n");
+
+  } 
+  else {
+    Print(L"Couldn't validate the file to be an ELF executable\n");
+  }
+  
+  return EFI_SUCCESS;
+
+}
+
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
+
   InitializeLib(ImageHandle, SystemTable);
   SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
   
@@ -79,54 +150,26 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   Print(L"Welcome user to Seed OS\n");
 
 
-  //loading kernel declerations
-  EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
-  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
-  EFI_FILE_PROTOCOL *Root;
-  EFI_FILE_PROTOCOL *KernelFile;
-  EFI_FILE_INFO *fileInfo;
-  EFI_PHYSICAL_ADDRESS buffer;
+  EFI_PHYSICAL_ADDRESS *buffer;
   UINTN kernelSize;
   UINTN pages;
-   
-  // loading the kernel
-  Print(L"loading kernel onto memory\n");
-  SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
-  Print(L"loaded image\n");
-  SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem);
-  Print(L"passed image to file system\n");
-  FileSystem->OpenVolume(FileSystem, &Root);
-  Print(L"opened the file\n");
-  EFI_STATUS STATUS = Root->Open(Root, &KernelFile, L"kernel\\kernel.elf", EFI_FILE_MODE_READ, 0);
-  if (EFI_ERROR(STATUS)) {
-    Print(L"FAILED TO READ THE ELF FILE\n", STATUS);
-  }
-  Print (L"set file to read mode\n");
 
-  fileInfo = LibFileInfo(KernelFile);
-  if (fileInfo == NULL) {
-    Print(L"LibFileInfo returned NULL\n");
-  }
-  Print(L"got file info\n");
-  kernelSize = fileInfo->FileSize; 
-  Print(L"captured kernel size from info\n");
-  pages = (kernelSize + 0xFFF) / 0x1000;
-  SystemTable->BootServices->AllocatePages(AllocateAnyPages, EfiLoaderData, pages, &buffer);
-  KernelFile->Read(KernelFile, &kernelSize, (void *)buffer);
-  Print(L"Kernel loaded\n");
 
-  Print(L"parsing files for elf magic numebers\n");
-  // checking for the elf magic numbers
-  struct elf64_ehdr *elf = (struct elf64_ehdr *)buffer;
-  if (elf->e_ident[0] == 0x7F && elf->e_ident[1] == 'E' && elf->e_ident[2] == 'L' 
-      && elf->e_ident[3] == 'F'){
-    Print(L"File validated to be an ELF executable\n");
-
-  } 
-  else {
-    Print(L"Couldn't validate the file to be an ELF executable\n");
+  status = KernelLoader(ImageHandle, SystemTable, buffer, kernelSize, pages);
+  if (EFI_ERROR(status))
+  {
+    Print(L"Something went wrong: %r\n", status);
+    return status;
   }
   
+  struct elf64_ehdr *elf = (struct elf64_ehdr *)buffer;
+  status = ElfMagicCheck(elf);
+  if (EFI_ERROR(status))
+  {
+    Print(L"Something went wrong: %r\n", status);
+    return status;
+  }
+
   // load the progam segments
   Print(L"Parsing the program segments and outputting data\n");
   struct elf64_phdr *phdr = (struct elf64_phdr *)((char *)buffer + elf->e_phoff);
@@ -149,7 +192,13 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
       UINTN segPages = (phdr[i].p_memsz + 0xFFF) / 0x1000;
 
-      EFI_STATUS status = SystemTable->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, segPages, &segAddr);
+      status = SystemTable->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, segPages, &segAddr);
+       if (EFI_ERROR(status))
+        {
+          Print(L"Something went wrong: %r\n", status);
+          return status;
+        }
+
       CopyMem((void *)phdr[i].p_vaddr, (char *)buffer + phdr[i].p_offset, phdr[i].p_filesz);
       if (phdr[i].p_memsz > phdr[i].p_filesz) {
         SetMem((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), phdr[i].p_memsz - phdr[i].p_filesz, 0);
@@ -171,7 +220,7 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key);
 
   // getting memory map
-  EFI_STATUS status = GetSystemMemoryMap(SystemTable, &mapSize, &mapKey, &descriptorSize, &descriptorVersion, &MemoryMap);
+  status = GetSystemMemoryMap(SystemTable, &mapSize, &mapKey, &descriptorSize, &descriptorVersion, &MemoryMap);
   if (EFI_ERROR(status))
   {
     Print(L"Something went wrong: %r\n", status);
